2f15b4d8a6346ad9e71c9f3a3c72b56946e16c9a
valuntiny <459201296@qq.com>
2019-05-29 11:10:48 -0400

    preorder is [root, left, right]

--- Codec.py
+++ Codec.py
@@ -24,6 +24,16 @@ class Codec:
         :type root: TreeNode
         :rtype: str
         """
+        val = []
+
+        def preorder(root):
+            if root:
+                val.append(root.val)
+                preorder(root.left)
+                preorder(root.right)
+
+        preorder(root)
+        return ''.join(map(str, val))
 
     def deserialize(self, data):
         """Decodes your encoded data to tree.
@@ -31,6 +41,19 @@ class Codec:
         :type data: str
         :rtype: TreeNode
         """
+        data = list(map(int, data.split()))
+        stk = []
+        root = node = TreeNode[data[0]]
+        for n in data[1,]:
+            if n < node.val:
+                node.left = TreeNode[n]
+                stk.append(node)
+                node = node.left
+            else:
+                while stk and n > stk[-1].val:
+                    node = stk.pop()
+                node.right = TreeNode[n]
+                node = node.right
 
 # Your Codec object will be instantiated and called as such:
 # codec = Codec()
